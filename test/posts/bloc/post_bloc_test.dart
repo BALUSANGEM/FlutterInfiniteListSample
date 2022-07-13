56bb24fab6207401290908e383e190cd6bc2f11f
BALUSANGEM <vishnubalu.sangem@gmail.com>
2022-07-13 21:04:04 +0530

    Adds two unit tests Emits nothing when posts limit has reached Emits posts successfully when initial request is successful

--- test/posts/bloc/post_bloc_test.dart
+++ test/posts/bloc/post_bloc_test.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_infinite_list/posts/posts.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
@@ -5,16 +6,58 @@ import 'package:mocktail/mocktail.dart';
 
 class MockClient extends Mock implements http.Client {}
 
+Uri _postsUrl({required int start}) {
+  return Uri.https(
+    'jsonplaceholder.typicode.com',
+    '/posts',
+    <String, String>{'_start': '$start', '_limit': '20'},
+  );
+}
+
 void main() {
   group('Posts Bloc', () {
-
     late http.Client httpClient;
-    
-    setUp(() => {
-      httpClient = MockClient()
-    });
+
+    const List<Post> mockPosts = [
+      Post(id: 1, title: "Title", body: "This is mocked body")
+    ];
+
+    setUpAll(() => {registerFallbackValue(Uri())});
+
+    setUp(() => {httpClient = MockClient()});
     test('Initial state is PostState()', () {
       expect(PostBloc(httpClient: httpClient).state, const PostState());
+    });
+
+    group('PostFetched', () {
+      blocTest('Emits nothing when posts limit has reached ',
+          build: () => PostBloc(httpClient: httpClient),
+          seed: () => const PostState(hasReachedMax: true),
+          act: (PostBloc bloc) => bloc.add(PostFectched()),
+          expect: () => <PostState>[]);
+
+      blocTest(
+        'Emits posts successfully when initial request is successful ',
+        setUp: () {
+          when(() => httpClient.get(any())).thenAnswer((_) async {
+            return http.Response(
+              '[{ "id": 1, "title": "Title", "body": "This is mocked body" }]',
+              200,
+            );
+          });
+        },
+        build: () => PostBloc(httpClient: httpClient),
+        act: (PostBloc bloc) => bloc.add(PostFectched()),
+        expect: () => const <PostState>[
+          PostState(
+              status: PostStatus.success,
+              posts: mockPosts,
+              hasReachedMax: false)
+        ],
+        verify: (_) {
+          verify(() => httpClient.get(_postsUrl(start: 0))).called(1);
+        },
+      );
     });
   });
 }
