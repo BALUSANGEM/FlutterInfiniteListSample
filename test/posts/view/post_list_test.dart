41bdfaf5aab646b0a32a2ae034c20b13bc232201
BALUSANGEM <vishnubalu.sangem@gmail.com>
2022-07-15 21:44:53 +0530

    Adds test 'When max not reached shows 5 posts and bottom loader'

--- test/posts/view/post_list_test.dart
+++ test/posts/view/post_list_test.dart
@@ -2,6 +2,7 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_infinite_list/posts/posts.dart';
+import 'package:flutter_infinite_list/widgets/bottom_loader.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -12,6 +13,9 @@ void main() {
   late PostBloc bloc;
   setUp(() => {bloc = MockPostBlock()});
 
+  final mockPosts = List.generate(
+      5, (index) => Post(id: index, title: "Title", body: "This is body"));
+
   group('PostList', () {
     testWidgets('When post status is initial show circular progressbar',
         (tester) async {
@@ -20,16 +24,24 @@ void main() {
           home: BlocProvider.value(value: bloc, child: const PostsList())));
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
     });
-    
-    testWidgets('When post status is success but posts list is empty', (tester) async {
+
+    testWidgets('When post status is success but posts list is empty',
+        (tester) async {
       when(() => bloc.state).thenReturn(const PostState(
-        status: PostStatus.success,
-        posts: [],
-        hasReachedMax: false
-      ));
+          status: PostStatus.success, posts: [], hasReachedMax: false));
       await tester.pumpWidget(MaterialApp(
           home: BlocProvider.value(value: bloc, child: const PostsList())));
       expect(find.text('No posts'), findsOneWidget);
+    });
+
+    testWidgets('When max not reached shows 5 posts and bottom loader',
+        (tester) async {
+      when(() => bloc.state)
+          .thenReturn(PostState(status: PostStatus.success, posts: mockPosts));
+      await tester.pumpWidget(MaterialApp(
+          home: BlocProvider.value(value: bloc, child: const PostsList())));
+      expect(find.byType(PostListItem), findsNWidgets(5));
+      expect(find.byType(BottomLoader), findsOneWidget);
     });
   });
 }
