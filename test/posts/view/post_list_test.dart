dfcc50aecb9e84e20246a07c40a4076fed594662
BALUSANGEM <vishnubalu.sangem@gmail.com>
2022-07-15 21:36:02 +0530

    Adds test 'When post status is success but posts list is empty'

--- test/posts/view/post_list_test.dart
+++ test/posts/view/post_list_test.dart
@@ -1,7 +1,6 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_infinite_list/posts/bloc/post_bloc.dart';
 import 'package:flutter_infinite_list/posts/posts.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -20,6 +19,17 @@ void main() {
       await tester.pumpWidget(MaterialApp(
           home: BlocProvider.value(value: bloc, child: const PostsList())));
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    });
+    
+    testWidgets('When post status is success but posts list is empty', (tester) async {
+      when(() => bloc.state).thenReturn(const PostState(
+        status: PostStatus.success,
+        posts: [],
+        hasReachedMax: false
+      ));
+      await tester.pumpWidget(MaterialApp(
+          home: BlocProvider.value(value: bloc, child: const PostsList())));
+      expect(find.text('No posts'), findsOneWidget);
     });
   });
 }
